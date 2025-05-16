9cd506ab15ce33305cbb5e1d4c42b9194ea77bde
mahimasingh04 <mahiisinghh1802@gmail.com>
2025-05-16 23:51:29 +0530

    auth done

--- glow-backend/main.go
+++ glow-backend/main.go
@@ -4,14 +4,25 @@ import (
 	"github.com/gofiber/fiber/v2"
 	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
-
+  "os"
+    "github.com/joho/godotenv"
 	"github.com/mahimasingh04/go-website/glow-backend/models"
 	"github.com/mahimasingh04/go-website/glow-backend/routes"
 )
 
 func main() {
 	// Initialize DB
-	dsn := "host=localhost user=postgres password=Mahi91098$$ dbname=Skincare port=5432 sslmode=disable"
+
+	   err := godotenv.Load()
+    if err != nil {
+        panic("Error loading .env file")
+    }
+	dsn := "host=" + os.Getenv("DB_HOST") + 
+           " user=" + os.Getenv("DB_USER") + 
+           " password=" + os.Getenv("DB_PASSWORD") + 
+           " dbname=" + os.Getenv("DB_NAME") + 
+           " port=" + os.Getenv("DB_PORT") + 
+           " sslmode=" + os.Getenv("DB_SSLMODE")
 	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
 	if err != nil {
 		panic("Failed to connect to database")
